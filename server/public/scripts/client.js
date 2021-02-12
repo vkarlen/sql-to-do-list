b65f2c0ca0d7324e549e4acd8dd3f66b3dd8669b
Vada Karlen <vada.karlen@gmail.com>
2021-02-11 21:37:39 -0600

    Wired files so everyone is talking to eachother

---
+++ server/public/scripts/client.js
@@ -0,0 +1,7 @@
+console.log('JS');
+
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('JQ');
+}
---
+++ server/routes/task_router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const pool = require('../modules/pool');
+
+module.exports = router;
