915bc1f850114c336c2873bb10a893c5f49db180
Vada Karlen <vada.karlen@gmail.com>
2021-02-11 22:22:18 -0600

    Created POST and connected to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,27 @@
-console.log('JS');
-
 $(document).ready(onReady);
 
 function onReady() {
-  console.log('JQ');
+  $('#submitBtn').on('click', addTask);
+}
+
+function addTask() {
+  //console.log('in addTask');
+
+  const newTask = {
+    task: $('#taskIn').val(),
+  };
+  // console.log(newTask);
+
+  $.ajax({
+    method: 'POST',
+    url: '/tasks/',
+    data: { task: $('#taskIn').val() },
+  })
+    .then((res) => {
+      console.log('Back from server');
+    })
+    .catch((err) => {
+      console.log('Task not added', err);
+      alert('Task not added. Try again later.');
+    });
 }
--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -5,3 +5,9 @@ const pg = require('pg');
 const pool = require('../modules/pool');
 
 module.exports = router;
+
+router.post('/', (req, res) => {
+  console.log('in router');
+
+  res.send('oki doke');
+});
