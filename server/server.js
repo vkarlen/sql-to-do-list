a75b1337a80c2cf0857b6a1a65abc4e10ed8629b
Vada Karlen <vada.karlen@gmail.com>
2021-02-14 20:00:20 -0600

    Corrected the port for heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ app.use('/tasks', taskRouter);
 // static page
 app.use(express.static('server/public'));
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(port, function () {
   console.log("I'm in.", port);
 });
